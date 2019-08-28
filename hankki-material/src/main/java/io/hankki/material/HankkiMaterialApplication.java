7d6432c76702b18f34b524bfa4cb05359db66777
Wonhyung Park <wonhyung@Wonhyungui-MacBook-Pro.local>
2019-08-28 11:53:57 +0900

    프로젝트 생성

---
+++ hankki-material/src/main/java/io/hankki/material/HankkiMaterialApplication.java
@@ -0,0 +1,13 @@
+package io.hankki.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankkiMaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankkiMaterialApplication.class, args);
+	}
+
+}
---
+++ hankki-myrecipe/src/main/java/io/hankki/myrecipe/HankkiMyrecipeApplication.java
@@ -0,0 +1,13 @@
+package io.hankki.myrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankkiMyrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankkiMyrecipeApplication.class, args);
+	}
+
+}
