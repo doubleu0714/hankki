8d5a43917fff1f235bea21ec49608dc783461070
eunbyul0221 <eunbyul0221@gmail.com>
2019-08-29 11:24:30 +0900

    qeurydsl 관련 설정 주석 다시 해제, 카테고리별 검색 기능 추가

--- hankki-material/src/test/java/io/hankki/material/HankkiMaterialApplicationTests.java
+++ hankki-material/src/test/java/io/hankki/material/HankkiMaterialApplicationTests.java
@@ -1,16 +1,56 @@
 package io.hankki.material;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.hankki.material.domain.model.Material;
+import io.hankki.material.domain.model.MaterialCategory;
+import io.hankki.material.domain.repository.MaterialRepository;
+import lombok.extern.slf4j.Slf4j;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class HankkiMaterialApplicationTests {
+	
+	@Autowired
+	MaterialRepository repository;
+	
+	
+	@Before
+	public void setup() {
+		log.info("BEFORE TEST");
+		Material material = new Material();
+		material.setMaterialCategory(MaterialCategory.양념);
+		material.setMaterialName("참기름");
+		
+		repository.save(material);
+		
+	}
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void findByMaterialCategoryTest() {
+		log.info("findByMaterialCategoryTest START");
+		List<Material> materialList = repository.findByMaterialCategory(MaterialCategory.양념);
+		for(Material material: materialList) {
+			log.info(material.toString());
+		}
+		assertThat(materialList.size(), not(0));
+		
+		log.info("findByMaterialCategoryTest END");
 	}
 
 }
