5824f3abed23e4afd60d41fb6825670d85f7aa5f
mcdony <mcdony1114@gmail.com>
2019-08-29 17:41:27 +0900

    Test, recipi sql

--- hankki-recipe/src/main/java/io/hankki/recipe/domain/repository/RecipeRepository.java
+++ hankki-recipe/src/main/java/io/hankki/recipe/domain/repository/RecipeRepository.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
 
 import io.hankki.recipe.domain.model.Recipe;
 import io.hankki.recipe.domain.model.RecipeCategoryType;
@@ -13,14 +16,17 @@ import io.hankki.recipe.domain.model.RecipeMaterial;
 
 
 @RepositoryRestResource 
-public interface RecipeRepository extends JpaRepository<Recipe, Long>{
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
  	
  	 //1. 카테고리별로 조회하는 쿼리
 //	@Query("select a from Recipe a where recipeCategoryType = :recipeCategoryType")
 	List<Recipe> findByRecipeCategoryType(@Param("recipeCategoryType") RecipeCategoryType recipeCategoryType);
 	
+	
+	//2. 식재료별로 조회하는 쿼리
 	List<Recipe> findByMaterials_MaterialId(@Param("materialId") Long materialId);
 	
+
 	 //2. 식재료 별로 조회하는 쿼리
 //	@Query("select a from Recipe a where category = :category")
 //	List<Recipe> findBymaterialName (@Param("materialName") String materialName);
--- hankki-recipe/src/test/java/io/hankki/recipe/HankkiRecipeApplicationTests.java
+++ hankki-recipe/src/test/java/io/hankki/recipe/HankkiRecipeApplicationTests.java
@@ -1,16 +1,46 @@
 package io.hankki.recipe;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.hankki.recipe.domain.model.Recipe;
+import io.hankki.recipe.domain.model.RecipeCategoryType;
+import io.hankki.recipe.domain.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
+@Transactional
 public class HankkiRecipeApplicationTests {
 
+	@Autowired
+	RecipeRepository repository;
+	
 	@Test
 	public void contextLoads() {
 	}
+	@Test
+	public void findByRecipeCategoryTypeTest () {
+		log.info("findByRecipeCategoryType  START");
+		List<Recipe> recipeList = repository.findByRecipeCategoryType(RecipeCategoryType.한식);
+		
+		for(Recipe recipe: recipeList) {
+			log.info(recipe.toString());
+		}
+		
+		assertThat(recipeList.size(), not(0));
 
+		log.info("findByRecipeCategoryType END");
+	}
 }
