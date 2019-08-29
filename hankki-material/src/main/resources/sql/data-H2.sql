7f9ad121fae1f7befea2a11d4b406b4c52c4652c
eunbyul0221 <eunbyul0221@gmail.com>
2019-08-29 12:09:00 +0900

    초기입력 데이터 추가

---
+++ hankki-material/src/main/resources/sql/data-H2.sql
@@ -0,0 +1,1 @@
+insert into Material (materialId, materialCategory, materialName) values (null, '양념', '참기름')
--- hankki-material/src/test/java/io/hankki/material/HankkiMaterialApplicationTests.java
+++ hankki-material/src/test/java/io/hankki/material/HankkiMaterialApplicationTests.java
@@ -10,7 +10,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.hankki.material.domain.model.Material;
 import io.hankki.material.domain.model.MaterialCategory;
@@ -25,17 +28,6 @@ public class HankkiMaterialApplicationTests {
 	@Autowired
 	MaterialRepository repository;
 	
-	
-	@Before
-	public void setup() {
-		log.info("BEFORE TEST");
-		Material material = new Material();
-		material.setMaterialCategory(MaterialCategory.양념);
-		material.setMaterialName("참기름");
-		
-		repository.save(material);
-		
-	}
 
 	@Test
 	public void contextLoads() {
