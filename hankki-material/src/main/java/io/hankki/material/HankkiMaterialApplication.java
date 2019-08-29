d5f701b7ddf2de26267adf896639aa5d56545479
eunbyul0221 <eunbyu0221@gmail.com>
2019-08-29 10:35:38 +0900

    Swagger 및  Repository Rest 구성을 위한  Lib 및 설정

--- hankki-material/src/main/java/io/hankki/material/HankkiMaterialApplication.java
+++ hankki-material/src/main/java/io/hankki/material/HankkiMaterialApplication.java
@@ -2,14 +2,17 @@ package io.hankki.material;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RestController
 @EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
 public class HankkiMaterialApplication {
 
 	@GetMapping("hello")
